76db23303e5d15f038ad463a51671a0100f9de7c
Bisan09 <147491062+Bisan09@users.noreply.github.com>
2025-03-13 10:33:49 +0100

    Add files via upload

---
+++ Test/personatst/Persona.java
@@ -0,0 +1,98 @@
+package code;
+
+import java.util.function.BooleanSupplier;
+
+public class Persona {
+
+	public static final Object[] PESO_IDEAL = null;
+	public static final Object[] INFRAPESO = null;
+	public static Object[] SOBREPESO;
+
+	public Persona(String string, int i, char c, double d, int j) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Persona() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Persona(String string, int i, char c, double d, double e) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void setPeso(double d) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setAltura(double d) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setNombre(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Object getNombre() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void setEdad(int i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public int getEdad() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public String getDNI() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public BooleanSupplier esValidoDNI() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void setSexo(char c) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Object getSexo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object[] calcularIMC() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object visualizarGeneraLetraDNI(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public double getPeso() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public BooleanSupplier esMayorDeEdad() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public double getAltura() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
